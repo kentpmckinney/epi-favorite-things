315b7613ee8dfca5440eb743b304ae956e01bb03
Student <example@example.com>
2020-01-28 20:39:19 -0800

    add function to retrieve and append input.

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,10 +1,20 @@
 window.onload = (function() {
-  //alert('js is working');
 
 
+  document.getElementById("form").addEventListener("submit", function(e) {
+    let thing = document.getElementById("thing").value;
+    //alert(thing);
 
-  document.getElementById("form").addEventListener("submit", function() {
-    alert("form has been submitted");
+    document.getElementById("thing").value = "";
+
+    let ul = document.getElementById("thingsList");
+    let li = document.createElement("li");
+    
+    li.innerText = thing;
+
+    ul.append(li);
+    e.preventDefault();
+ 
   });
 
 });
